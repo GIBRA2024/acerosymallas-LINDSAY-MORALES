a9b7152fc6159cff468d61125a3eb3ecf73ba601
Mateo Monroy <monroymateo1@gmail.com>
2025-05-04 13:05:04 -0500

    listo

---
+++ script.js
@@ -0,0 +1,19 @@
+function descargarContacto() {
+    const vcardData = `BEGIN:VCARD
+VERSION:3.0
+N:Morales Arroyave;Lindsay;;;
+FN:Lindsay Morales Arroyave
+TEL:+57 3207533462
+EMAIL;TYPE=WORK: contabilidad@acerosymallas.com
+NOTE: Contadora
+END:VCARD`;
+
+    const blob = new Blob([vcardData], { type: 'text/vcard' });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'contacto.vcf';
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+}
