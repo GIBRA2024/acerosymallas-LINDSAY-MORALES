6a9a60c4be5ba00dfe49d0f8b33bf939d3c34dc1
GIBRA2024 <gibra.company@gmail.com>
2025-06-18 11:06:25 -0500

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ N:Morales Arroyave;Lindsay;;;
 FN:Lindsay Morales Arroyave
 TEL:+57 3207533462
 EMAIL;TYPE=WORK: contabilidad@acerosymallas.com
-NOTE: Contadora
+NOTE: Directora Financiera
 END:VCARD`;
 
     const blob = new Blob([vcardData], { type: 'text/vcard' });
